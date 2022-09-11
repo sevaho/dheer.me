23ffd7c129e4addacdb61fb55ebc050472dc8423
sevaho <sebastiaan@sevaho.io>
2022-09-11 17:08:33 +0200

    Init code

---
+++ src/lib/SocialButton.svelte
@@ -0,0 +1,13 @@
+<script charset="utf-8">
+	export let item = { name: 'Not Found' }; // default value
+</script>
+
+<div class="m-3">
+	<a
+      href="{item.href}"
+    title="Link to {item.name}"
+    class="md:w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-2 border-{item.color} hover:border-{item.color} hover:bg-{item.color} hover:text-white shadow-md py-2 px-6 inline-flex items-center"
+	>
+		<span class="mx-auto">{item.name}</span>
+	</a>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import SocialButton from '$lib/SocialButton.svelte';
+
+	let socials = [
+		{ name: 'Facebook', href: 'https://facebook.com', color: 'blue-500' },
+		{ name: 'Pintrest', href: 'https://pintrest.com', color: 'red-600' },
+		{ name: 'LinkedIn', href: 'https://linkedin.com', color: 'blue-600' }
+	];
+</script>
+
+<div class="flex items-center justify-center h-screen bg-white-200">
+	<div class="container">
+		<div class="text-center">
+			<h2
+				class="text-4xl tracking-tight leading-10 font-extrabold text-blue-900 sm:text-5xl sm:leading-none md:text-6xl"
+			>
+				D'heer <span class="text-gray-600">Architecture</span>
+			</h2>
+			<h3 class="text-xl md:text-3xl mt-10">Website under construction</h3>
+		</div>
+		<div class="flex flex-wrap mt-10 justify-center">
+			{#each socials as item}
+				<SocialButton {item} />
+			{/each}
+		</div>
+	</div>
+</div>
